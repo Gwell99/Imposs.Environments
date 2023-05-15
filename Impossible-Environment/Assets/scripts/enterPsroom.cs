a0b18db152572e650517d3bea484f301e0772807
wanerlllll <liw497@newschool.edu>
2023-05-14 21:05:39 -0400

    main-to-psroomtunnel

---
+++ Impossible-Environment/Assets/scripts/enterPsroom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enterPsroom : MonoBehaviour
+{
+    public string scenename;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(scenename);
+        }
+    }
+
+
+}
